67a945e6a6a457cb27955f5d418a1cdf0a007e2e
Fayiz <fayizcj94.com>
2023-08-17 13:27:27 +0530

    latest update

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 const jsonfile = require('jsonfile');
 const moment = require('moment');
-
 const simpleGit = require('simple-git');
 
-const FILE_PATH ='./data.json';
+const FILE_PATH = './data.json';
 
-const DATE =  moment().format();
+import('random').then(randomModule => {
+  const random = randomModule.default;
 
-const data ={
-    data: DATE
-
-}
-jsonfile.writeFile(FILE_PATH,data);
-
-simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE })
-
-
+  const makeCommit = n => {
+    if (n === 0) {
+      return simpleGit().push();
+    }
+  
+    const x = random.int(0, 54);
+    const y = random.int(0, 6);
+  
+    const DATE = moment()
+      .subtract(2, 'y')
+      .add(x, 'w')
+      .add(y, 'd')
+      .format();
+  
+    const data = {
+      date: DATE
+    };
+  
+    console.log(DATE);
+  
+    jsonfile.writeFile(FILE_PATH, data, () => {
+      simpleGit()
+        .add([FILE_PATH])
+        .commit(DATE, {'--date': DATE}, makeCommit.bind(this, n - 1));
+    });
+  };
+  
+  makeCommit(100);
+}).catch(error => {
+  console.error('Error importing random module:', error);
+});
